c7012ca1f2d8dfac0823a339a2f06ece269fd491
cdae6021 <cdae6021@yahoo.com.tw>
2022-05-19 20:34:02 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override') 
 const session = require('express-session')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -20,6 +21,14 @@ app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 
 usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.isAuthenticated()
+    res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+    res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+    next()
+  })
 app.use(routes)
 
 app.listen(3000, () => {
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const category = require('../../models/category')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
-   res.render('index')
-})
+   const userId = req.user._id   // 變數設定
+   Record.find({ userId })         // 加入查詢條件
+     .lean()
+     .sort({ _id: 'asc' })
+     .then(records => res.render('index', { records }))
+     .catch(error => console.error(error))
+ })
 
 module.exports = router
